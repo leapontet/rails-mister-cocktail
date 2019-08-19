dc6f9aa3b2630ffcc1869a8cd40e2dd595873308
Lea Pontet <lea.pontet@hotmail.fr>
2019-08-19 00:34:23 -0400

    Add dotenv - Protect my secret data in .env file

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,11 @@
 class CocktailsController < ApplicationController
-  before_action :set_cocktail, only: [ :show, :edit, :update ]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
@@ -20,25 +21,7 @@ class CocktailsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
-  def update
-    if @cocktail.update(cocktail_params)
-      redirect_to cocktail_path(@cocktail)
-    else
-      render :edit
-    end
-  end
-
-  # def destroy
-  # end
-
   private
-
-  def set_cocktail
-    @cocktail = Cocktail.find(params[:id])
-  end
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,5 @@
 class DosesController < ApplicationController
-    before_action :set_dose, only: [:show, :edit, :update, :destroy]
-  def index
-    @doses = Dose.all
-  end
 
-  def show
-  end
 
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
@@ -13,8 +7,8 @@ class DosesController < ApplicationController
   end
 
   def create
-    @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
@@ -23,28 +17,13 @@ class DosesController < ApplicationController
     end
   end
 
-  def edit
-  end
-
-  def update
-    if @dose.update(dose_params)
-      redirect_to dose_path(@dose)
-    else
-      render :edit
-    end
-  end
-
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    # redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
-
-  def set_dose
-    @dose = Dose.find(params[:id])
-  end
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
